caed21d01b639291cbee978a357d3eac4b0a5629
regard68 <hui3573237489@gmail.com>
2025-10-25 20:28:47 +0800

    update config

--- app.py
+++ app.py
@@ -43,4 +43,7 @@ def index():
     return render_template("index.html", weather=weather)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render 会自动提供 PORT
+    app.run(host="0.0.0.0", port=port)
+
